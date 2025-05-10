9c3649b7e33f9b1d60323d61289ad5550a94095e
ironsupr <abhinavsingh25001@gmail.com>
2025-05-10 17:23:11 +0530

    Added some basic tools

--- main.py
+++ main.py
@@ -6,13 +6,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+# @tool
+def calculator_tool(a:float, b:float) -> str:
+    """
+    Useful for performing basic arithmetic calculations.
+    """
+    # print(f"Tool Called: calculator_tool with arguments a={a}, b={b}")
+    return f"The result of adding {a} and {b} is {a + b}."
+
+def say_hello(name: str) -> str:
+    """
+    Useful for greeting a user.
+    """
+    return f"Hey {name}, how are you doing today?"
+
 def main():
     model = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0)
 
-    tools = []
+    tools = [calculator_tool, say_hello]
     agent_executor = create_react_agent(model, tools)
 
-    print("Welcome! I'm uoir AI agent. Type 'quit' to exit.")
+    print("Welcome! I'm a AI agent. Type 'quit' to exit.")
     print("You can ask me to perform calculations, answer questions, or provide information.")
 
     while True:
